07259fceb2c3b9cb6d229f5be7cf7d20795835a4
selva <selvaganapathy@outlook.in>
2017-07-01 18:26:32 +0530

    merged swipable changes

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,14 @@ var path = require("path")
 module.exports = {
   entry: {
     home: "./dist/features/city/components/HomeClient.js",
-    gods: "./dist/features/gods/components/GodClient.js"
+    gods: "./dist/features/gods/components/GodClient.js",
+    swipeable: "./dist/components/Swipeable.js"
   },
   output: {
     path: path.resolve(__dirname, "dist/assets"),
     filename: "[name].bundle.js"
   },
-          devtool: "cheap-module-source-map",
+  devtool: "cheap-module-source-map",
   module: {
     loaders: [
       {
